facec3206772b93c9d507fc9f6d5a3d6995d738c
i3rtt1 <carlosibertti@gmail.com>
2024-09-30 11:17:15 -0300

    se modifica vista lista

--- app_cuentos/urls.py
+++ app_cuentos/urls.py
@@ -1,8 +1,10 @@
 #
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('', views.lista_cuentos, name='lista_cuentos'),
+    path('', views.index, name='index'),  # Vista de inicio
+    # Vista de lista de cuentos
+    path('lista/', views.lista_cuentos, name='lista_cuentos'),
 ]
