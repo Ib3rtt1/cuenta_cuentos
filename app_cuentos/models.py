11587ad9bcf7c463f8865ab90a91f4423eb8419a
i3rtt1 <carlosibertti@gmail.com>
2024-09-30 10:03:40 -0300

    se crea modelo app_cuentos👷

--- app_cuentos/models.py
+++ app_cuentos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cuento(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=100)
+    contenido = models.TextField()
+    fecha_publicacion = models.DateField()
+
+    def __str__(self):
+        return self.titulo
--- app_cuentos/views.py
+++ app_cuentos/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Cuento
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'app_cuentos/index.html')
+
+
+def lista_cuentos(request):
+    cuentos = Cuento.objects.all()
+    return render(request, 'app_cuentos/lista_cuentos.html', {'cuentos': cuentos})
